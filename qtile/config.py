088bf543bd73276ae2792312d6b4fec39eba01b3
Priyanshu Verma <priyanv2003@gmail.com>
2021-07-05 10:48:44 +0530

    Updated color scheme

--- qtile/config.py
+++ qtile/config.py
@@ -218,10 +218,10 @@ colors = [["#282c34", "#282c34"], # panel background
           ["#3d3f4b", "#434758"], # background for current screen tab
           ["#ffffff", "#ffffff"], # font color for group names
           ["#ff5555", "#ff5555"], # border line color for current tab
-          ["#74438f", "#74438f"], # border line color for 'other tabs' and color for 'odd widgets'
-          ["#4f76c7", "#4f76c7"], # color for the 'even widgets'
-          ["#e1acff", "#e1acff"], # window name
-          ["#ecbbfb", "#ecbbfb"]] # backbround for inactive screens
+          ["#2e9e62", "#2e9e62"], # border line color for 'other tabs' and color for 'odd widgets'
+          ["#5a6bed", "#5a6bed"], # color for the 'even widgets'
+          ["#20bd7e", "#20bd7e"], # window name
+          ["#215ed9", "#215ed9"]] # backbround for inactive screens
 
 prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
 
