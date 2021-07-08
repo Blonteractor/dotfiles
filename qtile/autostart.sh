2021b1ce7d034468bb1e0c4354f270b4df5a9943
Priyanshu Verma <priyanv2003@gmail.com>
2021-07-08 10:40:35 +0530

    Made everything transparent for now

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 lxsession &
-picom --active-opacity=0.85 --experimental-backends &
+picom --active-opacity=0.85 --inactive-opacity=0.9 --experimental-backends &
 nitrogen --restore &
 volumeicon &
 nm-applet &
